47be288e2af5e693692f122b571640f4586965b7
Aleksandr Kostyrev <kostyrevaa@gmail.com>
2016-07-24 20:36:38 +0300

    Fix creating of kojiadmin in postgresql

--- files/insert_kojiadmin.sql
+++ files/insert_kojiadmin.sql
@@ -1,2 +1,5 @@
-insert into users (id, name, status, usertype) values (1, 'kojiadmin', 0, 0);
-insert into user_perms (user_id, perm_id, creator_id) values (1, 1, 1);
+insert into users (name, usertype, status) values ('kojiadmin', 0, 0);
+insert into user_perms (user_id, perm_id, creator_id)
+      select users.id, permissions.id, users.id from users, permissions
+      where users.name in ('kojiadmin')
+            and permissions.name = 'admin';
